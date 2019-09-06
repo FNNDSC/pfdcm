a17ccee96504328031b0e568729519460e9f8d68
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2019-09-06 17:04:12 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def readme():
 
 setup(
       name             =   'pfdcm',
-      version          =   '2.0.0.0',
+      version          =   '2.0.2.0',
       description      =   'Path and File DICOM',
       long_description =   readme(),
       author           =   'Rudolph Pienaar',
