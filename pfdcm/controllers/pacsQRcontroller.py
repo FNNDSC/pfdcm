270948a4586a702ec225cffc0ad0b3c97c442af7
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2022-07-22 13:49:42 -0400

    Add async/await to pypx.find call

--- pfdcm/controllers/pacsQRcontroller.py
+++ pfdcm/controllers/pacsQRcontroller.py
@@ -12,7 +12,7 @@ from    pydantic            import  BaseModel, Field
 from    typing              import  Optional, List, Dict
 
 from    .jobController      import  jobber
-
+import  asyncio
 import  subprocess
 from    models              import  pacsQRmodel
 import  logging
@@ -126,7 +126,7 @@ def pypx_multiprocessDo(
     return d_response
 
 
-def pypx_do(
+async def pypx_do(
         PACSobjName             : str,
         listenerObjName         : str,
         queryTerms              : pacsQRmodel.PACSqueryCore,
@@ -152,7 +152,7 @@ def pypx_do(
                                                 PACSobjName
                                             )
             d_service                       = d_PACSservice['info']
-            d_response['pypx']              = pypx.find({**d_service, **d_queryTerms})
+            d_response['pypx']              = await pypx.find({**d_service, **d_queryTerms})
             if d_response['pypx']['status'] == 'success':
                 d_response['status']        = True
                 d_response['message']       = "pypx.then = '%s' was executed successfully" % \
@@ -167,76 +167,3 @@ def pypx_do(
     #     json.dump(d_response, db)
     return d_response
 
-def QRS_do(
-        PACSobjName             : str,
-        listenerObjName         : str,
-        queryTerms              : pacsQRmodel.PACSqueryCore,
-        action                  : str   = "query"
-) -> dict:
-    """
-    Main dispatching method for performing either a:
-
-        * query
-        * retrieve
-        * status
-
-    as explicitly defined by the "action".
-    """
-    d_response  : dict  = {
-        'status'    :   False,
-        'find'      :   {},
-        'message'   :   "No %s performed" % action
-    }
-    d_service       : dict  = {}
-    d_queryTerms    : dict  = jsonable_encoder(queryTerms)
-    if PACSobjName in config.dbAPI.PACSservice_listObjs():
-        if listenerObjName in config.dbAPI.listenerService_listObjs():
-            d_listenerObj   : dict          = config.dbAPI.listenerService_info(
-                                                listenerObjName
-                                            )
-            d_PACSservice   : dict          = config.dbAPI.PACSservice_info(
-                                                PACSobjName
-                                            )
-            d_service                       = d_PACSservice['info']
-            d_service['executable']         = d_listenerObj['dcmtk']['info']['findscu']
-            if action == 'retrieve':
-                d_queryTerms['retrieve']    = True
-            d_response['find']              = pypx.find({**d_service, **d_queryTerms})
-            if d_response['find']['status'] == 'success':
-                d_response['status']        = True
-                d_response['message']       = "'%s' was executed successfully" % action
-        else:
-            d_response['message']       = \
-                "'%s' is not a configured listener service" % listenerObjName
-    else:
-        d_response['message']   = \
-                "'%s' is not a configured PACS service" % PACSobjName
-    return d_response
-
-def query_do(
-        PACSobjName             : str,
-        listenerObjName         : str,
-        query                   : pacsQRmodel.PACSqueryCore
-) -> dict:
-    d_response  : dict  = {
-        'status'    :   False,
-        'find'      :   {},
-        'message'   :   "No query performed"
-    }
-    d_service   : dict  = {}
-    d_query     : dict  = jsonable_encoder(query)
-    if PACSobjName in config.dbAPI.PACSservice_listObjs():
-        if listenerObjName in config.dbAPI.listenerService_listObjs():
-            d_listenerObj   : dict      = config.dbAPI.listenerService_info(
-                                            listenerObjName
-                                        )
-            d_PACSservice   : dict      = config.dbAPI.PACSservice_info(
-                                            PACSobjName
-                                        )
-            d_service                   = d_PACSservice['info']
-            d_service['executable']     = d_listenerObj['dcmtk']['info']['findscu']
-            d_response['find']          = pypx.find({**d_service, **d_query})
-    else:
-        d_response['message']   = "'%s' is not a configured PACS service" % \
-            PACSobjName
-    return d_response
