d10a342fd0ad32b78c591935317168aa12d7e615
Rudolph Pienaar <rudolph.pienaar@childrens.harvard.edu>
2022-11-03 19:43:21 -0400

    Initialise PACS services from filesystem, if pacs.json exists in the services directory

--- pfdcm/main.py
+++ pfdcm/main.py
@@ -15,15 +15,64 @@ from    routes.pacsQRrouter     import router   as pacsQR_router
 from    routes.smdbSetupRouter  import router   as smdbSetup_router
 # from    routes.foobarRouter     import router   as foobar_router
 from    os                      import path
-
 import  pfstate
 from    pfstate         import  S
+
+import  pudb
+from    pypx                import  smdb
+from    argparse            import  Namespace
+import  json
+import  config
+
+from    db                  import pfdb
 
 with open(path.join(path.dirname(path.abspath(__file__)), 'ABOUT')) as f:
     str_about       = f.read()
 
 with open(path.join(path.dirname(path.abspath(__file__)), 'VERSION')) as f:
     str_version     = f.read().strip()
+
+
+"""
+Perform some initialization for the `pfdcm` ecosystem. Most notably, this
+reads the PACS descriptor file, typically called `pacs.json` in the `pfdcm`
+log and services directory, typically /home/dicom/services/pacs.json
+
+Historically, the PACS information was "initialized" into `pfdcm` post
+startup. This of course was not replica-friendly since each `pfdcm` would
+on "start up" also still need to be initialized with all the PACS information.
+A re-start "safe" approach would be for the PACS information state to not
+reside only in `pfdcm` memory, but to be accessible from some external source,
+like a file or database.
+
+Each restart of `pfdcm` simply "initializes" itself with this external state
+information.
+
+Internally, `pfdcm` has a "db" called "pfdb.py" to keep track/state of PACS
+information (as well as some others). Furthermore, `pfdcm` also has access
+to the stateless (from `pfdcm` perspective) database of the pypx tools,
+"smdb".
+
+The solution here is to:
+
+    * if a file called `pacs.json` exists, read using pypx/smdb
+        * then, for each pacs in the file, "initialize" pfdb.py with
+          this information.
+    * else, do nothing more really.
+
+For completeness sake, "adding" more PACS information to a pfdcm using its
+API should also preserve/save this outside of `pfdcm` memory.
+"""
+# no doubt the logDir should be read from some config...
+# pudb.set_trace()
+SMDB    = smdb.SMDB(
+                Namespace(str_logDir = '/home/dicom/log')
+            )
+d_pacs  = SMDB.service_keyAccess('pacs')
+if d_pacs['status']:
+    for service in list(d_pacs['pacs']['services'].keys()):
+        d_info      = d_pacs['pacs']['services'][service]['info']
+        d_pfdcmPACS = config.dbAPI.PACSservice_initObj(service, d_info)
 
 tags_metadata = [
     {
